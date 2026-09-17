8a94dbb8674a28d80ce9095788b3828d06bec95d
Anshika Batham <bathamanshika23@gmail.com>
2026-09-17 12:13:36 +0530

    Updated array method to use student data objrcts

---
+++ practice-code/array-methods/script.js
@@ -0,0 +1,24 @@
+function studentdata(){
+    let students =["Anshika","Simran","Annie"];
+    console.log("original array:",students);
+
+    students.push("Shreya");
+    console.log("push():",students);
+
+    students.pop();
+    console.log("pop():",students);
+
+    students.unshift("Mannu");
+    console.log("unshift():",students);
+
+    students.shift();
+    console.log("shift():",students);
+
+    students.splice(1,2);
+    console.log("splice():",students);
+
+    students.slice(2,3);
+    console.log("slice():",students);
+}
+
+studentdata();
